14f45440c11812b958bb4fb6d05739ee084db559
Ethan Granato <ethan@granato.net>
2018-06-07 12:33:22 -0600

    main feed styling near complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FeedComponent } from './components/feed/feed.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
