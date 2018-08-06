9bbe315e84c6fff0760f227dd1ce77ee7bb98f13
Ethan Granato <ethang@vertechgroup.com>
2018-08-06 14:47:44 -0600

    minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+    const origin = window.location.origin;
+    const path = window.location.pathname;
+    if (origin.indexOf('https://') !== -1) {
+      if (origin.indexOf('www.') === -1) {
+        window.location.href = `https://www.projectmpenzi.com${path}`;
+      }
+    } else {
+      window.location.href = `https://www.projectmpenzi.com${path}`;
+    }
+  }
 }
